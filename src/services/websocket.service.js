cb071092e8425f16a01a225cfa22fe6586f52fdf
Chirag Chavda <chiragchavda2003@gmail.com>
2024-03-10 11:03:06 +0530

    server url changed

--- src/services/websocket.service.js
+++ src/services/websocket.service.js
@@ -1,7 +1,8 @@
 import io from "socket.io-client";
 
 function WsService() {
-  return io.connect("http://192.168.1.135:3001");
+  // return io.connect("https://tic-tac-toe-server-rouge.vercel.app");
+  return io.connect("https://spontaneous-rhea-daiict.koyeb.app/");
 }
 
 export default WsService;
