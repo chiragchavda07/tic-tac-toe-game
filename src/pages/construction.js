3db23d53cfe844427f8a59321f1a34862f5d84ce
Chirag Chavda <chiragchavda2003@gmail.com>
2024-03-11 00:16:52 +0530

    construction page modified

--- src/pages/construction.js
+++ src/pages/construction.js
@@ -2,39 +2,27 @@
 import "../styles/construction.css";
 import img from "../assets/IMG_9258.jpg";
 function Construction() {
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "white",
-    height: "100vh",
-  };
-
-  const divStyle1 = {
-    color: "black",
-    height: "100%",
-    marginTop: "100px",
-    fontFamily: "cursive",
-    fontSize: "50px",
-  };
-
   return (
-    <div style={containerStyle}>
-      <div style={divStyle1}>Under construction</div>
-      <div class="loader">
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
-        <div class="loader-square"></div>
+    <div className="containerStyle">
+      <div
+        style={{
+          height: "1000px",
+          marginTop: "50px",
+          objectFit: "cover",
+          filter: "blur(2px)",
+        }}
+      >
+        <img src={img} style={{ height: "100%" }} />
       </div>
-      <div style={{ height: "500px", marginTop: "100px"  }}>
-        <img
-          src={img}
-          style={{ height: "100%", backdropFilter: "blur(100px)" }}
-        />
+      <div className="divstyle1">Under construction</div>
+      <div class="loader loaderstyle">
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
       </div>
     </div>
   );
