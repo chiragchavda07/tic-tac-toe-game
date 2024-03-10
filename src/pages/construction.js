34e845c082b7b9080528c93deef2d0f1a0ba538e
Chirag Chavda <chiragchavda2003@gmail.com>
2024-03-10 23:40:58 +0530

    construction page modified

--- src/pages/construction.js
+++ src/pages/construction.js
@@ -1,14 +1,17 @@
 // import react from "react";
 import "../styles/construction.css";
+import img from "../assets/IMG_9258.jpg";
 function Construction() {
   const containerStyle = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    backgroundColor: "white",
+    height: "100vh",
   };
 
   const divStyle1 = {
-    color: "white",
+    color: "black",
     height: "100%",
     marginTop: "100px",
     fontFamily: "cursive",
@@ -18,7 +21,6 @@ function Construction() {
   return (
     <div style={containerStyle}>
       <div style={divStyle1}>Under construction</div>
-      {/* <div style={divStyle2}>Coming soon...</div> */}
       <div class="loader">
         <div class="loader-square"></div>
         <div class="loader-square"></div>
@@ -27,6 +29,12 @@ function Construction() {
         <div class="loader-square"></div>
         <div class="loader-square"></div>
         <div class="loader-square"></div>
+      </div>
+      <div style={{ height: "500px", marginTop: "100px"  }}>
+        <img
+          src={img}
+          style={{ height: "100%", backdropFilter: "blur(100px)" }}
+        />
       </div>
     </div>
   );
