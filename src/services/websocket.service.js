1e753da42edc712dc0cc108e7adc084435d39678
FGC-Chirag-Chavda <chirag.chavda@fintechglobal.center>
2024-03-19 11:24:35 +0530

    server url changed

--- src/services/websocket.service.js
+++ src/services/websocket.service.js
@@ -2,8 +2,8 @@ import io from "socket.io-client";
 
 function WsService() {
   // return io.connect("https://tic-tac-toe-server-rouge.vercel.app/");
-  return io.connect("http://localhost:3000");
-  // return io.connect("https://spontaneous-rhea-daiict.koyeb.app/");
+  // return io.connect("http://localhost:3000");
+  return io.connect("https://spontaneous-rhea-daiict.koyeb.app/");
 }
 
 export default WsService;
